3fac311d52d04e9857c46000b8436974c8845bf7
kwinkich <vankord21@gmail.com>
2023-12-19 00:59:22 +0300

    first commit

---
+++ frontend/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <div class="container">
+                <div class="header__content">
+                    <p class="logo">kwinkichBlog.</p>
+                    <nav class="header__menu">
+                        <li class="header__menu-item">
+                            <a href="#">Записи</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href="#">Головна</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href="#">Про мене</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href="#">Проєкти</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href="#">Навички</a>
+                        </li>
+                        <li class="header__menu-item">
+                            <a href="#">Контакти</a>
+                        </li>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <main class="posts">
+            <div class="container">
+                <div class="posts__content">
+                    <div class="posts__block">
+                        <h1 class="posts__title">Recent posts about my life</h1>
+                        <div class="post">
+                            <h2 class="post__title">Title</h2>
+                            <p class="post__text">Post text hahahah lorem ipsum bla figa ti super mega kruti() Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, amet non debitis minus maxime magni.</p>
+                            <div class="post__link-block">
+                                <a href="#" class="post__link">See full persik</a>
+                            </div>
+                        </div>
+                        <div class="post">
+                            <h2 class="post__title">Title</h2>
+                            <p class="post__text">Post text hahahah lorem ipsum bla figa ti super mega kruti()</p>
+                            <div class="post__link-block">
+                                <a href="#" class="post__link">See full persik</a>
+                            </div>
+                        </div>
+                        <div class="post">
+                            <h2 class="post__title">Title</h2>
+                            <p class="post__text">Post text hahahah lorem ipsum bla figa ti super mega kruti()</p>
+                            <div class="post__link-block">
+                                <a href="#" class="post__link">See full persik</a>
+                            </div>
+                        </div>
+                        <div class="post">
+                            <h2 class="post__title">Title</h2>
+                            <p class="post__text">Post text hahahah lorem ipsum bla figa ti super mega kruti()</p>
+                            <div class="post__link-block">
+                                <a href="#" class="post__link">See full persik</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__block">
+                        <h1 class="news__title">Recent news</h1>
+                        <div class="new">
+                            <h2 class="new__title">Title</h2>
+                            <p class="new__text">new text hahahah lorem ipsum bla figa ti super mega kruti() Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, amet non debitis minus maxime magni.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <?php
+        
+    ?>
+</body>
+</html>
